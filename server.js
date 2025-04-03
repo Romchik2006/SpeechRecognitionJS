25eef0a97db9b64f77aced78d1a73489a62ef2c3
User <Romiko.kvantidze@eu.edu.ge>
2025-04-03 14:31:59 +0400

    change some detail

--- server.js
+++ server.js
@@ -5,26 +5,27 @@ if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
     recognition.interimResults = true;
+    recognition.lang = 'en-US'; // შერჩეული ენა
 
     let p = document.createElement("p");
 
     recognition.addEventListener("result", (e) => {
-        texts.appendChild(p);
+        // ხანდახან `p` უნდა შექმნათ მხოლოდ მაშინ, როცა ნაჩვენებია ახალი ფინალური ტექსტი
         const text = Array.from(e.results)
-            .map((result) => result[0])
-            .map((result) => result.transcript)
+            .map((result) => result[0].transcript)
             .join("");
 
-        // recognition.lang = 'EN';
-
-        p.innerText = text;
-
-        if (e?.results[0].isFinal) {
-            p = document.createElement("p");
+        if (e.results[0].isFinal) {
+            texts.appendChild(p);
+            p.innerText = text;
+            p = document.createElement("p"); // ქმნის ახალ პარაგრაფს შემდეგი ტექსტისთვის
+        } else {
+            p.innerText = text; // მიმდინარე სიტყვები (interim results)
         }
     });
 
-    recognition.addEventListener("end", (e) => {
+    recognition.addEventListener("end", () => {
+        // თავიდან იწყებს "SpeechRecognition"-ის სესიის გახსნას
         recognition.start();
         feedback.innerText = "Listening...";
     });
@@ -35,7 +36,8 @@ if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
 
     recognition.addEventListener("error", (e) => {
         console.error("Speech recognition error:", e.error);
-        // Handle "no-speech" error
+
+        // თუ არ არის ქმედება, ან არ არის დალაგებული საუბრები
         if (e.error === "no-speech" || !e.results) {
             feedback.innerText = "No speech detected. Please try again.";
         } else {
@@ -43,13 +45,18 @@ if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
         }
     });
 
+    // კликების მოსმენა მხოლოდ მაშინ თუ ჯერ არ არის მოქმედებაში
     document.body.addEventListener("click", () => {
-        recognition.start();
+        if (recognition.running === false) {
+            recognition.start();
+        }
         feedback.innerText = "Listening...";
     });
 
+    // დაწყება
     recognition.start();
 } else {
     console.error("SpeechRecognition is not supported in this browser.");
     feedback.innerText = "Speech recognition is not supported in this browser.";
 }
+
